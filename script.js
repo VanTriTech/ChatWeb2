547b24bdc62a0eadede0f8f6d025a293da24faf7
VanTriTech <dovantri24012003@gmail.com>
2024-12-02 18:00:39 +0700

    Update script.js
    Signed-off-by: VanTriTech <dovantri24012003@gmail.com>

--- script.js
+++ script.js
@@ -412,7 +412,7 @@ function loadPosts() {
         return true; // Hiển thị tất cả posts không có @meme ở tab Timeline
     });
     
-    posts.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+    posts.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
     
     // Thêm posts vào DOM
     filteredPosts.forEach(post => {
